b70d170c88ac61a9eae458b647a628991435b1d5
AlparslanTR <bybluestht@gmail.com>
2024-09-30 12:11:28 +0300

    Proje dosyası ekle.

---
+++ Week-1/Program.cs
@@ -0,0 +1,6 @@
+﻿
+string UserName = "Alparslan Akbaş";
+string PaticaContent = "Patika da yer alma sebebim referans aracılığıyla güzel bir yazılım şirketine veya hayalim deki şirkete yerleşip profesyonel olarak kariyer inşa etmek..";
+
+Console.WriteLine(UserName);
+Console.WriteLine(PaticaContent);
