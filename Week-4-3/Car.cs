0f7270c6309b96f2000608295e9d2875b0b5a5e5
AlparslanTR <bybluestht@gmail.com>
2024-10-17 01:17:11 +0300

    Week-4.3
    Added encapsulation

---
+++ Week-4-3/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_3
+{
+    public class Car
+    {
+        // string ifadelerinizin nullable uyarı vermesi sizin kontrol etmenizi söyler, 
+        // string? şeklinde ifade ederseniz değişkenin boş da olabileceğini ifade edersiniz. 
+        // required derseniz değişkeniniz değer almak zorunda demiş olursunuz.
+
+
+        public required string Mark { get; set; } // = string.Empty; veya string.empty!; şeklinde de kullanabilirsiniz.
+        public required string Model { get; set; }
+        public string? Color { get; set; }
+        private int _doorCount { get; set; }
+
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _doorCount = value;
+                }
+
+                else
+                {
+                    _doorCount = -1;
+                    Console.WriteLine("Hata.! Kapı sayısı 2 veya 4 olabilir.!");
+                }
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine
+                (
+                    $"Marka: {Mark} \n"
+                    +
+                    $"Model: {Model} \n"
+                    +
+                    $"Color: {Color} \n"
+                    +
+                    $"Door Count: {DoorCount}"
+                );
+        }
+    }
+}
---
+++ Week-4-3/Program.cs
@@ -0,0 +1,21 @@
+﻿using Week_4_3;
+
+Car invalidCar = new()
+{
+    Mark = "Fiat Egea Cross",
+    Model = "Street",
+    Color = "Metal beyaz",
+    DoorCount = 3
+};
+invalidCar.GetInfo();
+
+Console.WriteLine();
+
+Car car = new()
+{
+    Mark = "Fiat Egea Cross",
+    Model = "Street",
+    Color = "Metal beyaz",
+    DoorCount = 4
+};
+car.GetInfo();  
