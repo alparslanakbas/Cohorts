dc9f3a8c378b81169b45a60b6f65e246929151b2
AlparslanTR <bybluestht@gmail.com>
2024-10-16 22:14:08 +0300

    Week-4.1
    Added ctor case

---
+++ Week-4-1/Baby.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_1
+{
+    public class Baby
+    {
+        public Baby()
+        {
+            Name = "Kayıp bebek";
+            Surname = "Bilinmiyor";
+            BirthDate = DateTime.UtcNow;
+        }
+
+        public Baby(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+            BirthDate = DateTime.UtcNow;
+            Console.WriteLine("Ingaaaaa");
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine
+                (
+                $"Name: {Name} \n"
+                +
+                $"Surname: {Surname} \n"
+                +
+                $"Birth Date: {BirthDate.ToLongDateString()}"               
+                ); 
+                
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ Week-4-1/Program.cs
@@ -0,0 +1,9 @@
+﻿using Week_4_1;
+
+Baby unkownBaby = new Baby();
+unkownBaby.GetInfo();
+
+Console.WriteLine();
+
+Baby myBaby = new Baby("Asena", "Akbaş");
+myBaby.GetInfo();
