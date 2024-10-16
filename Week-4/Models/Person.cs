07d085033ff0742aa696539813e9daf9fefaad9b
AlparslanTR <bybluestht@gmail.com>
2024-10-16 20:08:48 +0300

    Week-4
    Added OOP first case

---
+++ Week-4/Models/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4.Models
+{
+    public class Person
+    {
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public virtual string GetInfo()
+        {
+            return
+                $"Name: {Name} \n"
+                +
+                $"Surname: {Surname} \n"
+                +
+                $"BirthDate: {BirthDate.ToShortDateString()} \n"
+                ;
+        }
+    }
+
+    public class Student : Person
+    {
+        public bool IsStudent { get; set; }
+
+        public override string GetInfo() 
+        {
+            return base.GetInfo() + $"IsStudent: {IsStudent}";
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public bool IsTeacher { get; set; }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + $"Is Teacher: {IsTeacher}";
+        }
+    }
+
+}
---
+++ Week-4/Program.cs
@@ -0,0 +1,63 @@
+﻿using Week_4.Models;
+
+// Bu örnek pek de OOP ye uyum sağlamıyor çünkü person da hem öğretmen hem de öğrenci tutuyoruz sağlıklı değil
+//Person personStudent = new()
+//{
+//    Name = "Alparslan",
+//    Surname = "Akbaş",
+//    BirthDate = new DateTime(1999, 01, 03),
+//    IsStudent = true
+//};
+
+//Console.WriteLine
+//    (
+//        $"Öğrenci Bilgileri \n"
+//        + $"Name: {personStudent.Name} \n"
+//        + $"Surname: {personStudent.Surname} \n"
+//        + $"BirthDate: {personStudent.BirthDate.ToLongDateString()} \n"
+//        + $"IsStudent: {personStudent.IsStudent}"
+//    );
+
+//Console.WriteLine();
+
+//Person personTeacher = new()
+//{
+//    Name = "Hasan ali veli",
+//    Surname = "deli",
+//    BirthDate = new DateTime(1980, 01, 03),
+//    IsTeacher = true
+//};
+
+//Console.WriteLine
+//    (
+//        $"Öğretmen Bilgileri \n"
+//        + $"Name: {personTeacher.Name} \n"
+//        + $"Surname: {personTeacher.Surname} \n"
+//        + $"BirthDate: {personTeacher.BirthDate.ToLongDateString()} \n"
+//        + $"IsStudent: {personTeacher.IsStudent}"
+//    );
+
+
+// oop ye göre olması gereken
+
+Person student = new Student
+{
+    Name = "Alparslan",
+    Surname = "Akbaş",
+    BirthDate = new DateTime(1999, 01, 03),
+    IsStudent = true
+};
+
+Console.WriteLine($"-- Öğrenci Bilgileri --\n{student.GetInfo()}");
+
+Console.WriteLine();
+
+Person teacher = new Teacher
+{
+    Name = "Hasan Ali Veli",
+    Surname = "Deli",
+    BirthDate = new DateTime(1980, 01, 03),
+    IsTeacher = true
+};
+
+Console.WriteLine($"-- Öğretmen Bilgileri --\n{teacher.GetInfo()}");
