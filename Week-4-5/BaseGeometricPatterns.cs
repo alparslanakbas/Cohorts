aadb29e5bf9148b9f6a02beabbf71afd6c37d6ea
AlparslanTR <bybluestht@gmail.com>
2024-10-22 21:07:19 +0300

    Practice Polymorphism

---
+++ Week-4-5/BaseGeometricPatterns.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_5
+{
+    public class BaseGeometricPatterns
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public virtual double AreaCalculator() 
+        {
+            return Width * Height;
+        }
+    }
+
+
+    public class Square : BaseGeometricPatterns
+    {
+        public Square(double width) 
+        {
+            Width = width;
+            Height = width;
+        }
+    }
+
+    public class Rectangle : BaseGeometricPatterns
+    {
+        public Rectangle(double side, double height)
+        {
+            Width = side;
+            Height = height;
+        }
+    }
+
+    public class RightTriangle : BaseGeometricPatterns
+    {
+        public RightTriangle(double side, double height)
+        {
+            Width = side;
+            Height = height;
+        }
+
+        public override double AreaCalculator()
+        {
+            return (Width * Height) / 2;
+        }
+
+    }
+}
---
+++ Week-4-5/Program.cs
@@ -0,0 +1,9 @@
+﻿using Week_4_5;
+
+BaseGeometricPatterns square = new Square(4);
+BaseGeometricPatterns rectangle = new Rectangle(4, 8);
+BaseGeometricPatterns rightTriangle = new RightTriangle(4, 8);
+
+Console.WriteLine($"Karenin alanı: {square.AreaCalculator()}");
+Console.WriteLine($"Dikdörtgenin alanı: {rectangle.AreaCalculator()}");
+Console.WriteLine($"Dik üçgenin alanı: {rightTriangle.AreaCalculator()}");
