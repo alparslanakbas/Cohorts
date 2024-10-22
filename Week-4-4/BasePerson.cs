a9e745b9899da5b065849a8661793f9ad9bb7dca
AlparslanTR <bybluestht@gmail.com>
2024-10-22 20:20:53 +0300

    Pratice Inheritance

---
+++ Week-4-4/BasePerson.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_4
+{
+    public class BasePerson
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty!;
+        public string Surname { get; set; } = string.Empty!;
+    }
+
+
+    public class Student : BasePerson
+    {
+        public Guid StudentIdentifyNumber { get; set; }
+
+        public void WriteToConsoleForStudent()
+        {
+            Console.WriteLine($"{Id} Id'ye sahip Öğrencinin Adı: {FirstName} Soyadı: {Surname} Okul Numarası:{StudentIdentifyNumber}");
+        }
+    }
+
+
+    public class Teacher : BasePerson
+    {
+        public float Salary { get; set; }
+
+        public void WriteToConsoleForTeacher()
+        {
+            Console.WriteLine($"{Id} Id'ye sahip Öğretmenin Adı: {FirstName} Soyadı: {Surname} Maaş Bilgisi:{Salary}");
+        }
+    }
+
+
+    
+
+    
+
+}
---
+++ Week-4-4/Program.cs
@@ -0,0 +1,20 @@
+﻿using Week_4_4;
+
+Student student = new()
+{
+    Id = Guid.NewGuid(),
+    FirstName = "Alparslan",
+    Surname = "Akbaş",
+    StudentIdentifyNumber = Guid.NewGuid()
+};
+
+Teacher teacher = new()
+{
+    Id = Guid.NewGuid(),
+    FirstName = "Alparslan",
+    Surname = "Akbaş",
+    Salary = 85000.00f
+};
+
+student.WriteToConsoleForStudent();
+teacher.WriteToConsoleForTeacher();
