32ac9aa5de18941b980c1feb39ef50d9e38745fa
AlparslanTR <bybluestht@gmail.com>
2024-10-25 22:19:34 +0300

    Practice - Abstraction

---
+++ Week-4-6/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_6
+{
+    public abstract class Person
+    {
+        protected Person(string fullName, string? department)
+        {
+            FullName = fullName;
+            Department = department ?? "bilinmiyor"; // eğer departman null olabilir dersem ekstra kontrol etmem gerekir veya default bir tanımlama
+        }
+
+        public string FullName { get; set; }
+        public string? Department { get; set; } // burada değişkenimin null olabileceğini belirtiyorum.
+
+        public abstract void Pozition();
+
+        public void WriteToConsole()
+        {
+            Console.Write($"{FullName} adlı kişinin çalıştığı departman {Department} ");
+            Pozition();
+        }
+    }
+
+    public class Employee : Person
+    {
+        readonly string _pozisyon;
+        public Employee(string fullName, string? department, string pozisyon) : base(fullName, department) // base kullanıyorum çünkü personum yapıcı bir sınıf bu yüzden constructorunu almam lazım
+                                                                                                           // yoksa parametrelerin değerlerini alamam
+
+        {
+            _pozisyon = pozisyon;
+        }
+
+        public override void Pozition()
+        {
+            Console.WriteLine($"{_pozisyon} olarak çalışıyor.");
+        }
+    }
+
+}
---
+++ Week-4-6/Program.cs
@@ -0,0 +1,9 @@
+﻿using Week_4_6;
+
+Employee person = new("Alparslan Akbas", "Arge", "Software Developer");
+person.WriteToConsole();
+
+Console.WriteLine();
+
+Employee unknownPerson = new("Alparslan Akbas", null ,"Software Developer");
+unknownPerson.WriteToConsole();
