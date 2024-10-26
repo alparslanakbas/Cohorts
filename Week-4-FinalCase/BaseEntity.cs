63c22f050c3ea7e9902e0d3d243dbb1875267865
AlparslanTR <bybluestht@gmail.com>
2024-10-26 19:34:23 +0300

    Week-4 final app

---
+++ Week-4-FinalCase/BaseEntity.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_FinalCase
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity(string name, string? description, string? operationSystem)
+        {
+            Name = name;
+            Description = description;
+            OperationSystem = operationSystem;
+        }
+
+        public string Name { get; set; } = string.Empty!;
+        public string? Description { get; set; }
+        public Guid SerialNumber { get; set; } = Guid.NewGuid();
+        public string? OperationSystem { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public void WriteToConsoleCommonInfo()
+        {
+            Console.WriteLine
+                (
+                    $"Model Seri Numarası: {SerialNumber}\n" +
+                    $"Model Adı:  {Name}\n" +
+                    $"Model Açıklaması:  {Description}\n" +
+                    $"Model İşletim Sistemi:  {OperationSystem}\n" +
+                    $"Model Üretim Tarihi:  {CreatedDate}"
+                );
+        }
+
+    }
+
+    public class Computer : BaseEntity
+    {
+        private int _usbPortCount;
+        private bool _isBluetooth;
+        public Computer(string name, string? description, string? operationSystem, bool isBluetooth, int usbPortCount) : base(name, description, operationSystem)
+        {
+            _isBluetooth = isBluetooth;
+            UsbPortCount = usbPortCount;
+        }
+
+        public int UsbPortCount
+        {
+            get => _usbPortCount;
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _usbPortCount = value;
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz, USB giriş sayısı 2 veya 4 olmalıdır, çıkış yapılıyor..");
+                    _usbPortCount = -1;
+                    Environment.Exit(0);
+                }
+            }
+        }
+        public void WriteToConsoleComputerInfo()
+        {
+            WriteToConsoleCommonInfo();
+            Console.WriteLine($"USB giriş sayısı: {UsbPortCount}");
+            Console.WriteLine($"Model Bluetooth özelliği: {(_isBluetooth ? "Var" : "Yok")}");
+        }
+
+    }
+
+    public class Phone : BaseEntity
+    {
+        private bool _isLicenceTR;
+        public Phone(string name, string? description, string? operationSystem, bool isLicenceTR) : base(name, description, operationSystem)
+        {
+            _isLicenceTR = isLicenceTR;
+        }
+
+        public void WriteToConsolePhoneInfo()
+        {
+            WriteToConsoleCommonInfo();
+            Console.WriteLine($"Model TR dil desteği: {(_isLicenceTR ? "Var" : "Yok")}");
+        }
+    }
+
+}
---
+++ Week-4-FinalCase/Program.cs
@@ -0,0 +1,88 @@
+﻿using Week_4_FinalCase;
+
+bool keepGoing = true;
+while (keepGoing)
+{
+    Console.WriteLine("Telefon üretmek için 1, bilgisayar üretmek için 2'ye basın:");
+    string input = Console.ReadLine() ?? "";
+
+    switch (input)
+    {
+        case "1":
+            AddPhone();
+            break;
+        case "2":
+            AddComputer(); 
+            break;
+        default:
+            Console.WriteLine("Geçersiz seçim, tekrar deneyin.");
+            continue; 
+    }
+
+    AgainTry:
+    Console.WriteLine("Tekrar ürün eklemek için E tuşlayın veya çıkmak için 0'a basın:");
+    string choice = Console.ReadLine() ?? "";
+
+    if (choice.Equals("E", StringComparison.OrdinalIgnoreCase))
+    {
+        keepGoing = true;
+    }
+    else if (choice.Equals("0"))
+    {
+        keepGoing = false;
+        Console.WriteLine("İyi günler!");
+    }
+    else
+    {
+        Console.WriteLine("Geçersiz seçim tekrar deneyin");
+        goto AgainTry;
+    }
+}
+
+void AddPhone()
+{
+    Console.Write("Telefon Adı: ");
+    string name = Console.ReadLine() ?? "";
+
+    Console.Write("Telefon Açıklaması: ");
+    string description = Console.ReadLine() ?? "";
+
+    Console.Write("Telefon İşletim Sistemi: ");
+    string operationSystem = Console.ReadLine() ?? "";
+
+    Console.Write("TR Lisanslı mı? Evet için E, Hayır için H yazın: ");
+    string isLicenceTR = Console.ReadLine() ?? "";
+    bool trLisansli = isLicenceTR.Equals("E", StringComparison.OrdinalIgnoreCase);
+
+    Phone phone = new Phone(name, description, operationSystem, trLisansli);
+    Console.WriteLine($"{name} adlı telefon başarıyla üretildi.\n");
+    phone.WriteToConsolePhoneInfo();
+}
+
+
+void AddComputer()
+{
+    Console.Write("Bilgisayar Adı: ");
+    string name = Console.ReadLine() ?? "";
+
+    Console.Write("Bilgisayar Açıklaması: ");
+    string description = Console.ReadLine() ?? "";
+
+    Console.Write("Bilgisayar İşletim Sistemi: ");
+    string operationSystem = Console.ReadLine() ?? "";
+
+    Console.Write("Bluetooth özelliği var mı? (Evet için E, Hayır için H yazın): ");
+    string isBluetooth = Console.ReadLine() ?? "";
+    bool bluetooth = isBluetooth.Equals("E", StringComparison.OrdinalIgnoreCase);
+
+    Console.Write("USB giriş sayısı (2 veya 4 olmalı): ");
+    int usbPortCount = int.Parse(Console.ReadLine() ?? "0"); 
+
+    Computer computer = new Computer(name, description, operationSystem, bluetooth, usbPortCount);
+    Console.WriteLine($"{name} adlı bilgisayar başarıyla üretildi.\n");
+    computer.WriteToConsoleComputerInfo(); 
+}
+
+
+
+
