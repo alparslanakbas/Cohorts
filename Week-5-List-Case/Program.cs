1710d370389b2b385fabeb3839fb22d273228583
AlparslanTR <bybluestht@gmail.com>
2024-10-27 00:00:00 +0300

    list case added

---
+++ Week-5-List-Case/Program.cs
@@ -0,0 +1,16 @@
+﻿List<string> coffees = new List<string>();
+
+for (int i = 1; i < 6; i++)
+{
+    Console.Write($"Kahve {i}: ");
+    string coffeeName = Console.ReadLine() ?? "latte";
+    coffees.Add(coffeeName);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Kahve İsimleri");
+foreach (var coffee in coffees)
+{
+    Console.WriteLine(coffee);
+}
