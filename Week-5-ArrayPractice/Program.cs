5b78be1e16da5c0b7490d36a5694e271aebc1c5a
AlparslanTR <bybluestht@gmail.com>
2024-10-26 22:43:27 +0300

    added input

--- Week-5-ArrayPractice/Program.cs
+++ Week-5-ArrayPractice/Program.cs
@@ -4,9 +4,14 @@ for (int i = 0; i < 10; i++)
 {
     naturalNumbers[i] = i;
 }
-// string join ifadesi 2 değer alır amacı dizide ki verileri belirtilen bi ifadeyle ayırmaktır. ilk istediği değerde nasıl ayırmak istiyorsunuz onu girin
-// 2. değer ise dizinizin bulunduğu değişkeni.. linq sorugusuyla basit yoldan sıralama yapın
-Console.WriteLine("Büyükten küçüğe: " + String.Join("-", naturalNumbers.OrderByDescending(x => x)));
+
+Console.Write("Bir sayı ekle: ");
+int newValue = int.Parse(Console.ReadLine() ?? "0");
+
+Array.Resize(ref naturalNumbers, 11);
+naturalNumbers[10] = newValue;
+
+
 
 int total = 0;
 foreach (int i in naturalNumbers)
@@ -15,3 +20,8 @@ foreach (int i in naturalNumbers)
 }
 
 Console.WriteLine($"Sayıların toplamı: {total}");
+
+
+// string join ifadesi 2 değer alır amacı dizide ki verileri belirtilen bi ifadeyle ayırmaktır. ilk istediği değerde nasıl ayırmak istiyorsunuz onu girin
+// 2. değer ise dizinizin bulunduğu değişkeni.. linq sorugusuyla basit yoldan sıralama yapın
+Console.WriteLine("Büyükten küçüğe: " + String.Join("-", naturalNumbers.OrderByDescending(x => x)));
