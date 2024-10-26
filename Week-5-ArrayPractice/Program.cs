9c4b647e4744cbae87a081b848163d206b0ef3cc
AlparslanTR <bybluestht@gmail.com>
2024-10-26 22:35:26 +0300

    Practice array

---
+++ Week-5-ArrayPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] naturalNumbers = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    naturalNumbers[i] = i;
+}
+// string join ifadesi 2 değer alır amacı dizide ki verileri belirtilen bi ifadeyle ayırmaktır. ilk istediği değerde nasıl ayırmak istiyorsunuz onu girin
+// 2. değer ise dizinizin bulunduğu değişkeni.. linq sorugusuyla basit yoldan sıralama yapın
+Console.WriteLine("Büyükten küçüğe: " + String.Join("-", naturalNumbers.OrderByDescending(x => x)));
+
+int total = 0;
+foreach (int i in naturalNumbers)
+{
+    total += i;
+}
+
+Console.WriteLine($"Sayıların toplamı: {total}");
