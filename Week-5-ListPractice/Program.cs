ff7dccad99b064022e696bd3c6cbed7e0ec953d3
AlparslanTR <bybluestht@gmail.com>
2024-10-26 23:22:13 +0300

    Practice list

---
+++ Week-5-ListPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿List<string> invitees = new List<string>
+{
+    "David Guetta", "Martin Garrix", "Armin van Buuren", "Tiësto", "Calvin Harris",
+    "Dimitri Vegas & Like Mike", "Afrojack", "Skrillex", "Steve Aoki", "Marshmello"
+};
+
+foreach (var (name, index) in invitees.Select((name, index) => (name, index + 1)))
+{
+    Console.WriteLine($"{index}- {name}");
+}
