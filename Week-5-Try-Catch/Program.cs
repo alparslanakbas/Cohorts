b87b1770ba9b193baf5eb00320e17ba9ee4a170d
AlparslanTR <bybluestht@gmail.com>
2024-10-28 01:17:53 +0300

    Added try catch

---
+++ Week-5-Try-Catch/Program.cs
@@ -0,0 +1,10 @@
+﻿try
+{
+    Console.Write("Karesini öğrenmek istediğiniz rakamı girin: ");
+    double number = double.Parse(Console.ReadLine());
+    Console.WriteLine($"{number} sayısının karesi: " + Math.Pow(number,2));
+}
+catch (FormatException ex)
+{
+    Console.WriteLine($"Geçersiz giriş! Lütfen bir sayı giriniz. Hata: {ex}");
+}
