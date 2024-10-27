484215dc923073f4be21fa2ce7ef710e7671dc82
AlparslanTR <bybluestht@gmail.com>
2024-10-28 00:10:21 +0300

    OOP films app

--- Week-5-ImdbApp/Film.cs
+++ Week-5-ImdbApp/Film.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Week_5_ImdbApp
+﻿namespace Week_5_ImdbApp
 {
     public class Film
     {
@@ -16,5 +10,52 @@ namespace Week_5_ImdbApp
             Name = name;
             Imdb = imdb;
         }
+
+
+        public static void ListAllFilms(List<Film> filmList)
+        {
+            Console.WriteLine("--Tüm Filmler--");
+
+            foreach (var film in filmList)
+            {
+                Console.WriteLine($"Film Adı: {film.Name}, Imdb Puanı: {film.Imdb}");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void FilterByImdb(List<Film> filmList)
+        {
+            Console.WriteLine("Imdb puanı 4 ile 9 arasında olan filmler:");
+            var filteredFilms = filmList.Where(f => f.Imdb >= 4 && f.Imdb <= 9);
+
+            foreach (var film in filteredFilms)
+            {
+                Console.WriteLine($"Film Adı: {film.Name}, Imdb Puanı: {film.Imdb}");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void FilterByName(List<Film> filmList)
+        {
+            Console.WriteLine("İsmi A ile başlayan filmler:");
+            var filteredFilms = filmList.Where(f => f.Name.StartsWith("A", StringComparison.OrdinalIgnoreCase));
+
+            if (!filteredFilms.Any())
+            {
+                Console.WriteLine("A ile başlayan film yoktur");
+            }
+            else
+            {
+                foreach (var film in filteredFilms)
+                {
+                    Console.WriteLine($"Film Adı: {film.Name}, Imdb Puanı: {film.Imdb}");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
     }
 }
--- Week-5-ImdbApp/Program.cs
+++ Week-5-ImdbApp/Program.cs
@@ -1,63 +1,53 @@
 ﻿using Week_5_ImdbApp;
 
-        List<Film> filmList = new List<Film>();
-        bool keepGoing = true;
+List<Film> filmList = new List<Film>();
+bool keepGoing = true;
 
-        while (keepGoing)
-        {
-            AddFilm(filmList);
+while (keepGoing)
+{
+    AddFilm(filmList);
+    RollBack:
+    Console.Write("Yeni bir film eklemek istiyorsanız lütfen e tuşlayın, çıkış için h: ");
+    string answer = Console.ReadLine() ?? "h";
 
-            Console.Write("Yeni bir film eklemek istiyorsanız lütfen e tuşlayın, çıkış için h");
-            string answer = Console.ReadLine() ?? "";
-            keepGoing = answer.Equals("E", StringComparison.OrdinalIgnoreCase);
-        }
-
-        // Tüm filmleri listele
-        Console.WriteLine("Tüm Filmler:");
-        foreach (var film in filmList)
-        {
-            Console.WriteLine($"Film Adı: {film.Name}, Imdb Puanı: {film.Imdb}");
-        }
-
-        // Imdb puanı 4 ile 9 arası filmleri listele
-        Console.WriteLine("Imdb Puanı 4 ile 9 Arasında Olan Filmler:");
-        var filterFilmByImdb = filmList.Where(f => f.Imdb >= 4 && f.Imdb <= 9);
-        foreach (var film in filterFilmByImdb)
-        {
-            Console.WriteLine($"Film Adı: {film.Name}, Imdb Puanı: {film.Imdb}");
-        }
-
-        // İsmi a ile başlayan fimleri listele
-        Console.WriteLine("İsmi 'A' ile Başlayan Filmler:");
-        var filterFilmByName = filmList.Where(f => f.Name.StartsWith("A", StringComparison.OrdinalIgnoreCase));
-        foreach (var film in filterFilmByName)
-        {
-            Console.WriteLine($"Film Adı: {film.Name}, Imdb Puanı: {film.Imdb}");
-        }
-
-    static void AddFilm(List<Film> filmList)
+    if (!answer.Equals("h", StringComparison.OrdinalIgnoreCase) || answer.Equals("e", StringComparison.OrdinalIgnoreCase))
     {
-        Console.Write("Eklemek istediğiniz filmin adını gir: ");
-        string name = Console.ReadLine() ?? "Interstellar";
-
-        double imdb;
-        while (true)
-        {
-            Console.Write($"{name} adlı filmin IMDB puanını giriniz: ");
-            if (double.TryParse(Console.ReadLine(), out imdb))
-            {
-                break;
-            }
-            else
-            {
-                Console.WriteLine("Lütfen geçerli bir IMDB puanı giriniz.(Örnek- 10,0) ");
-            }
-        }
-
-        Film film = new Film(name, imdb);
-        filmList.Add(film);
-
-        Console.WriteLine($"Film Eklendi -> Adı: {film.Name}, IMDB Puanı: {film.Imdb}\n");
+        
+        Console.WriteLine("Geçersiz, lütfen sadece 'e' veya 'h' tuşlayın.");
+        goto RollBack;
     }
+
+    keepGoing = answer.Equals("e", StringComparison.OrdinalIgnoreCase);
+}
+
+Film.ListAllFilms(filmList);
+Film.FilterByImdb(filmList);
+Film.FilterByName(filmList);
+
+static void AddFilm(List<Film> filmList)
+{
+    Console.Write("Eklemek istediğiniz filmin adını giriniz: ");
+    string name = Console.ReadLine() ?? "Interstellar";
+
+    double imdb;
+    while (true)
+    {
+        Console.Write($"{name} adlı filmin IMDB puanını giriniz: ");
+        if (double.TryParse(Console.ReadLine(), out imdb))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Lütfen geçerli bir IMDB puanı giriniz.(Örnek- 9,5) ");
+        }
+    }
+
+    Film film = new Film(name, imdb);
+    filmList.Add(film);
+
+    Console.WriteLine();
+    Console.WriteLine($"Film Eklendi => Adı: {film.Name}, IMDB Puanı: {film.Imdb}\n");
+}
 
 
