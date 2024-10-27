e63860b6fe269c72b60c8073dde46693035b8d2f
AlparslanTR <bybluestht@gmail.com>
2024-10-28 00:00:35 +0300

    Added Film app

---
+++ Week-5-ImdbApp/Film.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5_ImdbApp
+{
+    public class Film
+    {
+        public string Name { get; set; }
+        public double Imdb { get; set; }
+
+        public Film(string name, double imdb)
+        {
+            Name = name;
+            Imdb = imdb;
+        }
+    }
+}
---
+++ Week-5-ImdbApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using Week_5_ImdbApp;
+
+        List<Film> filmList = new List<Film>();
+        bool keepGoing = true;
+
+        while (keepGoing)
+        {
+            AddFilm(filmList);
+
+            Console.Write("Yeni bir film eklemek istiyorsanız lütfen e tuşlayın, çıkış için h");
+            string answer = Console.ReadLine() ?? "";
+            keepGoing = answer.Equals("E", StringComparison.OrdinalIgnoreCase);
+        }
+
+        // Tüm filmleri listele
+        Console.WriteLine("Tüm Filmler:");
+        foreach (var film in filmList)
+        {
+            Console.WriteLine($"Film Adı: {film.Name}, Imdb Puanı: {film.Imdb}");
+        }
+
+        // Imdb puanı 4 ile 9 arası filmleri listele
+        Console.WriteLine("Imdb Puanı 4 ile 9 Arasında Olan Filmler:");
+        var filterFilmByImdb = filmList.Where(f => f.Imdb >= 4 && f.Imdb <= 9);
+        foreach (var film in filterFilmByImdb)
+        {
+            Console.WriteLine($"Film Adı: {film.Name}, Imdb Puanı: {film.Imdb}");
+        }
+
+        // İsmi a ile başlayan fimleri listele
+        Console.WriteLine("İsmi 'A' ile Başlayan Filmler:");
+        var filterFilmByName = filmList.Where(f => f.Name.StartsWith("A", StringComparison.OrdinalIgnoreCase));
+        foreach (var film in filterFilmByName)
+        {
+            Console.WriteLine($"Film Adı: {film.Name}, Imdb Puanı: {film.Imdb}");
+        }
+
+    static void AddFilm(List<Film> filmList)
+    {
+        Console.Write("Eklemek istediğiniz filmin adını gir: ");
+        string name = Console.ReadLine() ?? "Interstellar";
+
+        double imdb;
+        while (true)
+        {
+            Console.Write($"{name} adlı filmin IMDB puanını giriniz: ");
+            if (double.TryParse(Console.ReadLine(), out imdb))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Lütfen geçerli bir IMDB puanı giriniz.(Örnek- 10,0) ");
+            }
+        }
+
+        Film film = new Film(name, imdb);
+        filmList.Add(film);
+
+        Console.WriteLine($"Film Eklendi -> Adı: {film.Name}, IMDB Puanı: {film.Imdb}\n");
+    }
+
+
