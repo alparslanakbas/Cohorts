bb972d0e04462dfd468f61d6b4f5cddc7e4788a3
AlparslanTR <bybluestht@gmail.com>
2024-10-28 21:59:20 +0300

    Added week 5 final app

---
+++ Week-5-FinalApp/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5_FinalApp
+{
+    public class Car
+    {
+        public Car(string marka, string model, string color, int doorCount)
+        {
+            Marka = marka;
+            Model = model;
+            Color = color;
+            DoorCount = doorCount;
+        }
+
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int DoorCount { get; set; }
+        public Guid SerialNumber { get; private set; } = Guid.NewGuid();
+        public DateTime CreatedDate { get; private set; } = DateTime.Now;
+
+    }
+}
---
+++ Week-5-FinalApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using Week_5_FinalApp;
+
+List<Car> cars = new List<Car>();
+bool keepGoing = true;
+
+while (keepGoing)
+{
+    Console.Write("Araba üretmek istiyor musunuz ? evet için e hayır için h tuşlayınız.. (e/h): ");
+    string answer = Console.ReadLine() ?? "";
+
+    if (answer.Equals("e", StringComparison.OrdinalIgnoreCase))
+    {
+        Add(cars);
+    }
+    else if (answer.Equals("h", StringComparison.OrdinalIgnoreCase))
+    {
+        keepGoing = false;
+    }
+    else
+    {
+        Console.WriteLine("Geçersiz giriş, lütfen 'e' veya 'h' giriniz.");
+    }
+}
+
+Console.WriteLine("\nOluşturulan Arabalar:");
+foreach (var car in cars)
+{
+    Console.WriteLine($"Seri numarası: {car.SerialNumber}, Marka: {car.Marka}");
+}
+        
+
+static void Add(List<Car> cars)
+{
+    Console.Write("Marka: ");
+    string marka = Console.ReadLine() ?? "";
+
+    Console.Write("Model: ");
+    string model = Console.ReadLine() ?? "";
+
+    Console.Write("Renk: ");
+    string color = Console.ReadLine() ?? "";
+
+    DoorCount:
+    int doorCount;
+    try
+    {
+        Console.Write("Kapı Sayısı: ");
+        doorCount = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Geçersiz kapı sayısı, lütfen rakam giriniz.");
+        goto DoorCount;
+    }
+
+    Car newCar = new Car(marka, model, color, doorCount);
+    cars.Add(newCar);
+
+    Console.WriteLine("Araba başarıyla eklendi!\n");
+}
