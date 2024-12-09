91cfc1f4f578dadbddb29d3b03481f8c3207230b
alparslanakbas <bybluestht@gmail.com>
2024-12-09 21:30:42 +0300

    Added
    Added code first relation mini app.

---
+++ Week-12-Code-First-2.Library/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Week_12_Code_First_2.Library.Models
+{
+    internal class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
